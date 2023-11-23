98c0c197b555477f661e0ae3e7584c2f6c356c81
ET <tacconellielisabetta50@gmail.com>
2023-11-23 15:15:16 +0100

    here's my first exercise on Inheritance

---
+++ src/Gatto.java
@@ -0,0 +1,7 @@
+public class Gatto extends Animale{
+    @Override
+    public void faiIlVerso() {
+        super.faiIlVerso();
+        System.out.println("Il verso del gatto è: miau miau!");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Animale animale = new Animale();
+        animale.faiIlVerso();
+        Gatto gatto = new Gatto();
+        gatto.faiIlVerso();
+    }
+}
